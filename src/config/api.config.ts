7a13b690c7850b891647bea9b18583c90b3bcc21
Gustavo Bessa <gbvirtual@gmail.com>
2018-07-05 19:37:14 -0300

    Creating structure

---
+++ src/config/api.config.ts
@@ -0,0 +1,5 @@
+export const API_CONFIG = {
+    baseUrl: "http://localhost:8080",
+    //baseUrl: "https://gb-curso-spring-ionic.herokuapp.com",
+    //bucketBaseURL: "https://s3-sa-east-1.amazonaws.com/gb-curso-spring-ionic"
+}
